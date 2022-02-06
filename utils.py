82d697ed26663458383a2be711857070f82d17de
Phil McLaughlin <philip.mclaughlin@statsperform.com>
2022-02-05 19:49:00 -0700

    cleanup effort, streamline code a lot, etc

---
+++ utils.py
@@ -0,0 +1,8 @@
+def timed(func):
+    @wraps(func)
+    def timed_func(*args, **kwargs):
+        start_time = datetime.now()
+        ret = func(*args, **kwargs)
+        print(f'{func.__name__} - {(datetime.now() - start_time).total_seconds():.4f}')
+        return ret
+    return timed_func
